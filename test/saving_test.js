c6ece9002bde45e165858314ebb1cd82de103a4e
Victor <victor.n.tran@outlook.com>
2018-09-13 04:31:02 -0400

    Rename file and change test descriptions

---
+++ test/saving_test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+// Describe tests
+describe("Saving records", () => {
+  // create tests
+
+  // it creates a single test
+  it("Saves a record to the database", () => {});
+});
