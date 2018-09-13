f77b8063e08675b2694ec45990f53d76652105d7
Victor <victor.n.tran@outlook.com>
2018-09-13 15:08:27 -0400

    fix bug in which test ran and tried to save data before we actually connect to database

--- test/connection.js
+++ test/connection.js
@@ -3,14 +3,20 @@ const mongoose = require("mongoose");
 // ES6 Promises
 mongoose.Promise = global.Promise;
 
-// connect to mongodb
-mongoose.connect("mongodb://localhost/testaroo");
+// Writing a hook to make sure we have a connection before running tests for CRUD
+// connect to db before test run
+before(done => {
+  // we pass done as a parameter because we need to know when this is done before we actually start a test
+  // connect to mongodb
+  mongoose.connect("mongodb://localhost/testaroo");
 
-// listen to this event, open, once
-mongoose.connection
-  .once("open", () => {
-    console.log("Connection has been made, now make fireworks...");
-  })
-  .on("error", error => {
-    console.log(error);
-  });
+  // listen to this event, open, once
+  mongoose.connection
+    .once("open", () => {
+      console.log("Connection has been made, now make fireworks...");
+      done();
+    })
+    .on("error", error => {
+      console.log(error);
+    });
+});
