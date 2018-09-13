80314a1b08caefe3952b103b89346014f02468f0
Victor <victor.n.tran@outlook.com>
2018-09-13 04:40:51 -0400

    wrote async methods -> then() and passed done into the cb of the it to call at the end of the async cb

--- test/saving_test.js
+++ test/saving_test.js
@@ -6,13 +6,21 @@ describe("Saving records", () => {
   // create tests
 
   // it creates a single test
-  it("Saves a record to the database", () => {
+  it("Saves a record to the database", done => {
+    // passed done into cb
     // create character
     const char = new MarioChar({
       name: "Mario"
     });
 
     // need to save to db
-    char.save(); // saves directly to the db
+    char
+      .save()
+      .then(() => {
+        assert(char.isNew === false); // not sure why I can't just negate the result of isNew
+        done(); // because it is async, we need to tell mocha the test is done
+      })
+      .catch(err => {}); // saves directly to the db
+    // save is async
   });
 });
