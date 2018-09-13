80ae9cf4d239bb6a08dbdb177e31a011e58b9984
Victor <victor.n.tran@outlook.com>
2018-09-13 04:34:06 -0400

    create instance of MarioChar and save();

--- test/saving_test.js
+++ test/saving_test.js
@@ -1,9 +1,18 @@
 const assert = require("assert");
+const MarioChar = require("../models/mariochar");
 
 // Describe tests
 describe("Saving records", () => {
   // create tests
 
   // it creates a single test
-  it("Saves a record to the database", () => {});
+  it("Saves a record to the database", () => {
+    // create character
+    const char = new MarioChar({
+      name: "Mario"
+    });
+
+    // need to save to db
+    char.save(); // saves directly to the db
+  });
 });
