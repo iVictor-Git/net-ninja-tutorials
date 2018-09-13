fb1ad417c68a1a2d8eef58deb8349f821244828e
Victor <victor.n.tran@outlook.com>
2018-09-13 04:29:43 -0400

    add notes as to what "it" does

--- test/demo_test.js
+++ test/demo_test.js
@@ -3,6 +3,8 @@ const assert = require("assert");
 // Describe tests
 describe("some demo tests", () => {
   // create tests
+
+  // it creates a single test
   it("adds two numbers together", () => {
     assert(2 + 3 === 5);
   });
