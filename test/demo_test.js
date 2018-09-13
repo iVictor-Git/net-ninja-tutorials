ebdaf986fd9074cac883236eda9195fd3c6aef6d
Victor <victor.n.tran@outlook.com>
2018-09-13 04:20:12 -0400

    write basic test

---
+++ test/demo_test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+// Describe tests
+describe("some demo tests", () => {
+  // create tests
+  it("adds two numbers together", () => {
+    assert(2 + 3 === 5);
+  });
+});
