34a7e637f6ef8dc35dd7dd7d1593b76bcd0bc5a9
Victor <victor.n.tran@outlook.com>
2018-09-14 02:45:56 -0400

    fix styling

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,14 +5,8 @@ const { GraphQLObjectType, GraphQLString } = graphql;
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {
-            type: GraphQLString
-        },
-        name: {
-            type: GraphQLString
-        },
-        genre: {
-            type: GraphQLString
-        }
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
     })
 });
