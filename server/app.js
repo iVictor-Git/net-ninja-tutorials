50f0001efe997be7bb06a86d3b20e74b73ab64cd
Victor <victor.n.tran@outlook.com>
2018-09-14 02:14:29 -0400

    Set up graphql

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const app = express();
+
+// set middleware
+app.use('/graphql', graphqlHTTP({}));
 
 const port = process.env.PORT || 4000;
 // app needs to listen to port
