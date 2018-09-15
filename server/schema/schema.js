27db861466195ef990c1ae87cbf3638b68aa81bb
Victor <victor.n.tran@outlook.com>
2018-09-14 21:18:19 -0400

    add authorId to book dummy data to do nested queries or relational data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 // dummy data
 const books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
 ];
 
 const authors = [
@@ -28,7 +28,16 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                // parent is the Book props
+                // name, genre, id, authorId
+                console.log(parent);
+                return _.find(authors, { id: parent.authorId });
+            }
+        }
     })
 });
 
