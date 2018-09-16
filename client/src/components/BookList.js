a0936cb6fb534f45769fcfd092beee03a7f938a2
iVictor-Git <Victor.n.tran.social@outlook.com>
2018-09-16 17:36:42 -0400

    changed id prop on <li>'s to be key

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -9,7 +9,7 @@ class BookList extends Component {
       return <div>Loading books...</div>;
     }
     return data.books.map(book => {
-      return <li id={book.id}>{book.name}</li>;
+      return <li key={book.id}>{book.name}</li>;
     });
   };
   render() {
