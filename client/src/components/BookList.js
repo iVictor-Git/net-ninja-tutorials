abd2b7200a810a82969207cddd5ed3de7943e01f
Victor tran <victor.n.tran.social@outlook.com>
2018-09-16 16:57:29 -0400

    write function to display data and render data

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -3,25 +3,31 @@ import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 
 const getBooksQuery = gql`
-    {
-        books {
-            name
-            id
-        }
+  {
+    books {
+      name
+      id
     }
+  }
 `;
 
 class BookList extends Component {
-    render() {
-        console.log(this.props);
-        return (
-            <div id="main">
-                <ul id="book-list">
-                    <li>Book</li>
-                </ul>
-            </div>
-        );
+  displayBooks = () => {
+    const { data } = this.props;
+    if (data.loading) {
+      return <div>Loading books...</div>;
     }
+    return data.books.map(book => {
+      return <li id={book.id}>{book.name}</li>;
+    });
+  };
+  render() {
+    return (
+      <div id="main">
+        <ul id="book-list">{this.displayBooks()}</ul>
+      </div>
+    );
+  }
 }
 
 export default graphql(getBooksQuery)(BookList);
